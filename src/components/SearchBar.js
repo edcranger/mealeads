933eb0e2ca65e9d74b24d05ad99778e1ce4a2db0
edcranger <edisonocampo.eo@gmail.com>
2021-04-06 21:09:46 +0800

    adding searchbar functionality

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,18 +4,18 @@ import AdvanceSearch from "./AdvanceSearch";
 import SimpleSearch from "./SimpleSearch";
 
 const SearchBar = () => {
-  const [isAdvanceSearch, setIsAdvanceSearch] = useState(true);
+  const [isAdvanceSearch, setIsAdvanceSearch] = useState(false);
 
   return (
     <div className="py-5  ">
       {isAdvanceSearch ? <AdvanceSearch /> : <SimpleSearch />}
       <div className=" py-1 w-full text-center">
-        <div
+        <a
           onClick={() => setIsAdvanceSearch(!isAdvanceSearch)}
-          className="text-yellow-700 w-full cursor-pointer"
+          className="text-yellow-700 w-60 cursor-pointer"
         >
           {isAdvanceSearch ? "Name Search" : "Advance Search"}
-        </div>
+        </a>
       </div>
     </div>
   );
--- src/components/SimpleSearch.js
+++ src/components/SimpleSearch.js
@@ -1,26 +1,103 @@
-import React from "react";
+import { useState, useEffect, useRef } from "react";
 import { FaSearch } from "react-icons/fa";
 
 const SimpleSearch = () => {
-  const searchSubmit = (e) => {
+  const [display, setDisplay] = useState(false);
+  const [options, setOptions] = useState(["wew"]);
+  const [search, setSearch] = useState("");
+  const searchBackRef = useRef(null);
+
+  useEffect(() => {
+    if (search.length < 1) {
+      setDisplay(false);
+    }
+  }, [search]);
+
+  const searchKey = async (e) => {
     e.preventDefault();
+    setSearch(e.target.value);
+    setDisplay(true);
   };
 
   return (
-    <form onSubmit={searchSubmit}>
-      <div className="flex flex-col md:flex-row items-center">
-        <div className="flex flex-col justify-center  md:flex-row w-full ">
+    <form onBlur={() => setDisplay(false)}>
+      <div className="flex flex-row justify-center items-center my-10">
+        <div className="flex justify-center w-3/4 absolute">
           <input
             type="text"
-            className="py-3 px-4 rounded md:rounded-r-none   bg-yellow-50  focus:outline-none border-2 border-blue-100 focus:border-yellow-400 "
+            ref={searchBackRef}
+            value={search}
+            onChange={searchKey}
+            className="py-3 px-4 rounded-l-lg w-full  bg-yellow-50  focus:outline-none border-2 border-blue-100 focus:border-yellow-400 "
             placeholder="Search Recipe"
           />
           <button
-            className="shadow focus:outline-none bg-blue-200 hover:bg-blue-100  mx-auto md:mx-0 mt-2 md:mt-0 h-14 md:h-auto rounded-md md:rounded-l-none w-38 md:w-10"
+            className=" focus:outline-none bg-blue-200 hover:bg-blue-100  rounded-r-lg w-10 "
             type="submit"
           >
-            <FaSearch className="w-20 h-8 md:h-6 md:w-10 mx-auto text-blue-300" />
+            <FaSearch className="w-10 h-6 mx-auto text-blue-300" />
           </button>
+          {display && options.length !== 0 && (
+            <div className=" absolute top-11 mt-2 w-full pl-2 pr-10 z-50  ">
+              <ul className="bg-white shadow border-2">
+                <li className=" flex items-center py-2  hover:bg-yellow-50 px-3 cursor-pointer ">
+                  <img
+                    src="https://spoonacular.com/recipeImages/782601-312x231.jpg"
+                    alt=""
+                    className="w-12 h-12"
+                  />
+                  <div className="flex flex-col ml-2 ">
+                    <div className="font-bold text-gray-700">Name</div>
+                    <div className="text-xs text-gray-500">Name</div>
+                  </div>
+                </li>
+                <li className=" flex items-center py-2  hover:bg-yellow-50 px-3 cursor-pointer ">
+                  <img
+                    src="https://spoonacular.com/recipeImages/782601-312x231.jpg"
+                    alt=""
+                    className="w-12 h-12"
+                  />
+                  <div className="flex flex-col ml-2 ">
+                    <div className="font-bold text-gray-700">Name</div>
+                    <div className="text-xs text-gray-500">Name</div>
+                  </div>
+                </li>
+                <li className=" flex items-center py-2  hover:bg-yellow-50 px-3 cursor-pointer ">
+                  <img
+                    src="https://spoonacular.com/recipeImages/782601-312x231.jpg"
+                    alt=""
+                    className="w-12 h-12"
+                  />
+                  <div className="flex flex-col ml-2 ">
+                    <div className="font-bold text-gray-700">Name</div>
+                    <div className="text-xs text-gray-500">Name</div>
+                  </div>
+                </li>
+                <li className=" flex items-center py-2  hover:bg-yellow-50 px-3 cursor-pointer ">
+                  <img
+                    src="https://spoonacular.com/recipeImages/782601-312x231.jpg"
+                    alt=""
+                    className="w-12 h-12"
+                  />
+                  <div className="flex flex-col ml-2 ">
+                    <div className="font-bold text-gray-700">Name</div>
+                    <div className="text-xs text-gray-500">Name</div>
+                  </div>
+                </li>
+                <li className=" flex items-center py-2  hover:bg-yellow-50 px-3 cursor-pointer ">
+                  <img
+                    src="https://spoonacular.com/recipeImages/782601-312x231.jpg"
+                    alt=""
+                    className="w-12 h-12"
+                  />
+                  <div className="flex flex-col ml-2 ">
+                    <div className="font-bold text-gray-700">Name</div>
+                    <div className="text-xs text-gray-500">Name</div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </form>
